8422d52fbcf9e2c2c9b1c3c313682649bc08bedc
starryzhang <i@vulpecula.cc>
2022-10-26 16:50:56 +0800

    test

---
+++ src/main/java/cc/vulpecula/domainbackend/DomainBackendApplication.java
@@ -0,0 +1,13 @@
+package cc.vulpecula.domainbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/vulpecula/domainbackend/DomainBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.vulpecula.domainbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
