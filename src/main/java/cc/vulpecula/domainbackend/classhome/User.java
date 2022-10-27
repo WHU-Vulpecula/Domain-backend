31ddc612bcc72cfcf73d48f0b5a979733eb741ae
StarryZhang-whu <93083801+StarryZhang-whu@users.noreply.github.com>
2022-10-27 16:02:42 +0800

    login&register

--- src/main/java/cc/vulpecula/domainbackend/classhome/User.java
+++ src/main/java/cc/vulpecula/domainbackend/classhome/User.java
@@ -1,4 +1,16 @@
 package cc.vulpecula.domainbackend.classhome;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("user_data")
 public class User {
+    private String userName;
+    private String password;
+    private String email;
+    @TableId(type= IdType.AUTO)
+    private Integer id;
 }
